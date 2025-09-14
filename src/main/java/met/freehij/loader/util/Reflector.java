89f77e3dd9af5b979831df9999540748b21487fc
this aint my pfp <sip22ssd@gmail.com>
2025-09-14 20:01:12 +0300

    chatgpt rewrote my reflector cuz im lazy af

--- src/main/java/met/freehij/loader/util/Reflector.java
+++ src/main/java/met/freehij/loader/util/Reflector.java
@@ -3,18 +3,18 @@ package met.freehij.loader.util;
 import met.freehij.loader.util.mappings.FieldMappings;
 import met.freehij.loader.util.mappings.MethodMappings;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Reflector {
-    private final HashMap<String, Reflector> fieldCaches = new HashMap<String, Reflector>();
-    private final HashMap<String, Reflector> methodCaches = new HashMap<String, Reflector>();
-    private final HashMap<String, Reflector> classCaches = new HashMap<String, Reflector>();
-    
+    private static final MethodHandles.Lookup LOOKUP = MethodHandles.lookup();
+    private static final Map<Class<?>, HandleCache> HANDLE_CACHES = new HashMap<>();
+
     private final Class<?> clazz;
     private final Object object;
 
@@ -69,6 +69,7 @@ public class Reflector {
 
     public Reflector getField(String name) {
         Class<?> current = this.getActualClass();
+        String originalName = name;
 
         while (current != null) {
             String mappedName = FieldMappings.get(current.getName().replace(".", "/"), name);
@@ -79,32 +80,32 @@ public class Reflector {
             current = current.getSuperclass();
         }
         if (current == null) {
-            throw new RuntimeException("No mapping found for " + name);
+            throw new RuntimeException("No mapping found for " + originalName);
         }
 
         try {
-            Field field = findField(current, name);
-            field.setAccessible(true);
-            Object value = field.get(object);
+            HandleCache cache = getHandleCache(current);
+            MethodHandle getter = cache.getFieldGetter(name);
+            Object value = cache.isFieldStatic(name) ? getter.invoke() : getter.invoke(object);
             return new Reflector(
-                    value != null ? value.getClass() : field.getType(),
+                    value != null ? value.getClass() : cache.getFieldType(name),
                     value
             );
-        } catch (Exception e) {
+        } catch (Throwable e) {
             throw new RuntimeException("Failed to get field: " + name, e);
         }
     }
 
     public Reflector getFieldRaw(String name) {
         try {
-            Field field = findField(this.getActualClass(), name);
-            field.setAccessible(true);
-            Object value = field.get(object);
+            HandleCache cache = getHandleCache(this.getActualClass());
+            MethodHandle getter = cache.getFieldGetter(name);
+            Object value = cache.isFieldStatic(name) ? getter.invoke() : getter.invoke(object);
             return new Reflector(
-                    value != null ? value.getClass() : field.getType(),
+                    value != null ? value.getClass() : cache.getFieldType(name),
                     value
             );
-        } catch (Exception e) {
+        } catch (Throwable e) {
             throw new RuntimeException("Failed to get field: " + name, e);
         }
     }
@@ -126,20 +127,28 @@ public class Reflector {
         }
 
         try {
-            Field field = findField(current, name);
-            field.setAccessible(true);
-            field.set(object, value);
-        } catch (Exception e) {
+            HandleCache cache = getHandleCache(current);
+            MethodHandle setter = cache.getFieldSetter(name);
+            if (cache.isFieldStatic(name)) {
+                setter.invoke(value);
+            } else {
+                setter.invoke(object, value);
+            }
+        } catch (Throwable e) {
             throw new RuntimeException("Failed to set field: " + name, e);
         }
     }
 
     public void setFieldRaw(String name, Object value) {
         try {
-            Field field = findField(this.getActualClass(), name);
-            field.setAccessible(true);
-            field.set(object, value);
-        } catch (Exception e) {
+            HandleCache cache = getHandleCache(this.getActualClass());
+            MethodHandle setter = cache.getFieldSetter(name);
+            if (cache.isFieldStatic(name)) {
+                setter.invoke(value);
+            } else {
+                setter.invoke(object, value);
+            }
+        } catch (Throwable e) {
             throw new RuntimeException("Failed to set field: " + name, e);
         }
     }
@@ -162,26 +171,25 @@ public class Reflector {
         }
 
         try {
-            return this.invokeRaw(methodName, parseDescriptor(descriptor), args);
+            Class<?>[] paramTypes = parseDescriptor(descriptor);
+            return this.invokeRaw(methodName, paramTypes, args);
         } catch (Exception exception) {
             throw new RuntimeException(exception);
         }
     }
 
-    public Reflector invokeRaw(String methodName, Class<?>[] descriptor, Object... args) {
+    public Reflector invokeRaw(String methodName, Class<?>[] paramTypes, Object... args) {
         try {
-            Method method = findMethod(
-                    object != null ? object.getClass() : clazz,
-                    methodName,
-                    descriptor
-            );
-            method.setAccessible(true);
-            Object result = method.invoke(object, args);
+            HandleCache cache = getHandleCache(object != null ? object.getClass() : clazz);
+            MethodHandle method = cache.getMethod(methodName, paramTypes);
+            Object result = cache.isMethodStatic(methodName, paramTypes) ?
+                    method.invokeWithArguments(args) :
+                    method.invokeWithArguments(prependObject(args));
             return new Reflector(
-                    result != null ? result.getClass() : method.getReturnType(),
+                    result != null ? result.getClass() : method.type().returnType(),
                     result
             );
-        } catch (Exception e) {
+        } catch (Throwable e) {
             throw new RuntimeException("Failed to invoke: " + methodName, e);
         }
     }
@@ -194,71 +202,34 @@ public class Reflector {
         }
     }
 
-    public Reflector newInstanceRaw(Class<?>[] descriptor, Object... args) {
+    public Reflector newInstanceRaw(Class<?>[] paramTypes, Object... args) {
         try {
-            Object instance = this.getActualClass().getConstructor(descriptor).newInstance(args);
+            HandleCache cache = getHandleCache(this.getActualClass());
+            MethodHandle constructor = cache.getConstructor(paramTypes);
+            Object instance = constructor.invokeWithArguments(args);
             return new Reflector(instance.getClass(), instance);
-        } catch (Exception exception) {
-            throw new RuntimeException(exception);
+        } catch (Throwable e) {
+            throw new RuntimeException("Failed to create new instance", e);
         }
-    }
-    
-    private static Field findFieldInternal(Class<?> searchClass, String name) {
-    	Class<?> current = searchClass;
-        while (current != null) {
-            try {
-                return current.getDeclaredField(name);
-            } catch (NoSuchFieldException e) {
-                current = current.getSuperclass();
-            }
-        }
-        throw new NoSuchFieldError("Field not found: " + name);
-    }
-    
-    private static Method findMethodInternal(Class<?> searchClass, String name, Class<?>[] paramTypes) {
-        Class<?> current = searchClass;
-        while (current != null) {
-            try {
-                return current.getDeclaredMethod(name, paramTypes);
-            } catch (NoSuchMethodException e) {
-                current = current.getSuperclass();
-            }
-        }
-        throw new NoSuchMethodError("Method not found: " + name);
-    }
-    
-    private static final boolean ENABLE_CACHE = true;
-    
-    private static Field findField(Class<?> searchClass, String name) {
-    	ClassCache cc;
-    	if(ENABLE_CACHE) {
-    		cc = ClassCache.get(searchClass);
-    		Field f = cc.field(name);
-    		if(f != null) return f;
-    	}
-    	
-    	Field f = findFieldInternal(searchClass, name);
-    	if(ENABLE_CACHE) {
-    		cc.cache(name, f);
-    	}
-    	return f;
-    }
-    private static Method findMethod(Class<?> searchClass, String name, Class<?>[] paramTypes) {
-    	ClassCache cc;
-    	if(ENABLE_CACHE) {
-    		cc = ClassCache.get(searchClass);
-    		Method m = cc.method(name, paramTypes);
-    		if(m != null) return m;
-    	}
-    	
-    	Method m = findMethodInternal(searchClass, name, paramTypes);
-    	if(ENABLE_CACHE) {
-    		cc.cache(name, m, paramTypes);
-    	}
-    	return m;
     }
 
-    public static Class<?>[] parseDescriptor(String descriptor) throws ClassNotFoundException {
+    private Object[] prependObject(Object[] args) {
+        if (args == null) {
+            return new Object[] { object };
+        }
+        Object[] newArgs = new Object[args.length + 1];
+        newArgs[0] = object;
+        System.arraycopy(args, 0, newArgs, 1, args.length);
+        return newArgs;
+    }
+
+    private static HandleCache getHandleCache(Class<?> clazz) {
+        synchronized (HANDLE_CACHES) {
+            return HANDLE_CACHES.computeIfAbsent(clazz, HandleCache::new);
+        }
+    }
+
+    private static Class<?>[] parseDescriptor(String descriptor) throws ClassNotFoundException {
         List<Class<?>> classes = new ArrayList<>();
         boolean isArray = false;
         for (int i = 0; i < descriptor.length(); i++) {
@@ -299,21 +270,186 @@ public class Reflector {
                     isArray = false;
                     break;
                 case 'L':
-                    String className = "";
-                    while (descriptor.charAt(i + 1) != ';') {
-                        className += descriptor.charAt(i + 1);
+                    StringBuilder className = new StringBuilder();
+                    while (i + 1 < descriptor.length() && descriptor.charAt(i + 1) != ';') {
+                        className.append(descriptor.charAt(i + 1));
                         i++;
                     }
-                    className = className.replace("/", ".");
-                    classes.add(isArray ? Array.newInstance(Class.forName(className),
-                            0).getClass() : Class.forName(className));
+                    String clsName = className.toString().replace("/", ".");
+                    classes.add(isArray ? Array.newInstance(Class.forName(clsName), 0).getClass() : Class.forName(clsName));
                     isArray = false;
                     break;
                 case ')':
                     return classes.toArray(new Class<?>[0]);
-
             }
         }
         return classes.toArray(new Class<?>[0]);
+    }
+
+    private static class HandleCache {
+        private final Class<?> clazz;
+        private final Map<String, MethodHandle> fieldGetters = new HashMap<>();
+        private final Map<String, MethodHandle> fieldSetters = new HashMap<>();
+        private final Map<MethodKey, MethodHandle> methods = new HashMap<>();
+        private final Map<ConstructorKey, MethodHandle> constructors = new HashMap<>();
+        private final Map<String, Boolean> fieldStaticFlags = new HashMap<>();
+        private final Map<String, Class<?>> fieldTypes = new HashMap<>();
+        private final Map<MethodKey, Boolean> methodStaticFlags = new HashMap<>();
+
+        HandleCache(Class<?> clazz) {
+            this.clazz = clazz;
+        }
+
+        MethodHandle getFieldGetter(String name) throws NoSuchFieldException, IllegalAccessException {
+            return fieldGetters.computeIfAbsent(name, n -> {
+                try {
+                    Field field = findField(clazz, n);
+                    field.setAccessible(true);
+                    fieldStaticFlags.put(n, Modifier.isStatic(field.getModifiers()));
+                    fieldTypes.put(n, field.getType());
+                    return LOOKUP.unreflectGetter(field);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        MethodHandle getFieldSetter(String name) throws NoSuchFieldException, IllegalAccessException {
+            return fieldSetters.computeIfAbsent(name, n -> {
+                try {
+                    Field field = findField(clazz, n);
+                    field.setAccessible(true);
+                    fieldStaticFlags.put(n, Modifier.isStatic(field.getModifiers()));
+                    fieldTypes.put(n, field.getType());
+                    return LOOKUP.unreflectSetter(field);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        MethodHandle getMethod(String name, Class<?>[] paramTypes) throws NoSuchMethodException, IllegalAccessException {
+            MethodKey key = new MethodKey(name, paramTypes);
+            return methods.computeIfAbsent(key, k -> {
+                try {
+                    Method method = findMethod(clazz, name, paramTypes);
+                    method.setAccessible(true);
+                    methodStaticFlags.put(key, Modifier.isStatic(method.getModifiers()));
+                    return LOOKUP.unreflect(method);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        MethodHandle getConstructor(Class<?>[] paramTypes) throws NoSuchMethodException, IllegalAccessException {
+            ConstructorKey key = new ConstructorKey(paramTypes);
+            return constructors.computeIfAbsent(key, k -> {
+                try {
+                    Constructor<?> constructor = clazz.getDeclaredConstructor(paramTypes);
+                    constructor.setAccessible(true);
+                    return LOOKUP.unreflectConstructor(constructor);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        boolean isFieldStatic(String name) {
+            return fieldStaticFlags.getOrDefault(name, false);
+        }
+
+        Class<?> getFieldType(String name) {
+            return fieldTypes.get(name);
+        }
+
+        boolean isMethodStatic(String name, Class<?>[] paramTypes) {
+            return methodStaticFlags.getOrDefault(new MethodKey(name, paramTypes), false);
+        }
+
+        private static Field findField(Class<?> searchClass, String name) throws NoSuchFieldException {
+            Class<?> current = searchClass;
+            while (current != null) {
+                try {
+                    return current.getDeclaredField(name);
+                } catch (NoSuchFieldException e) {
+                    current = current.getSuperclass();
+                }
+            }
+            throw new NoSuchFieldException("Field not found: " + name);
+        }
+
+        private static Method findMethod(Class<?> searchClass, String name, Class<?>[] paramTypes) throws NoSuchMethodException {
+            Class<?> current = searchClass;
+            while (current != null) {
+                try {
+                    return current.getDeclaredMethod(name, paramTypes);
+                } catch (NoSuchMethodException e) {
+                    current = current.getSuperclass();
+                }
+            }
+            throw new NoSuchMethodException("Method not found: " + name);
+        }
+    }
+
+    private static class MethodKey {
+        private final String name;
+        private final Class<?>[] paramTypes;
+
+        MethodKey(String name, Class<?>[] paramTypes) {
+            this.name = name;
+            this.paramTypes = paramTypes;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            MethodKey methodKey = (MethodKey) o;
+            if (!name.equals(methodKey.name)) return false;
+            if (paramTypes.length != methodKey.paramTypes.length) return false;
+            for (int i = 0; i < paramTypes.length; i++) {
+                if (!paramTypes[i].equals(methodKey.paramTypes[i])) return false;
+            }
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name.hashCode();
+            for (Class<?> paramType : paramTypes) {
+                result = 31 * result + paramType.hashCode();
+            }
+            return result;
+        }
+    }
+
+    private static class ConstructorKey {
+        private final Class<?>[] paramTypes;
+
+        ConstructorKey(Class<?>[] paramTypes) {
+            this.paramTypes = paramTypes;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ConstructorKey that = (ConstructorKey) o;
+            if (paramTypes.length != that.paramTypes.length) return false;
+            for (int i = 0; i < paramTypes.length; i++) {
+                if (!paramTypes[i].equals(that.paramTypes[i])) return false;
+            }
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = 1;
+            for (Class<?> paramType : paramTypes) {
+                result = 31 * result + paramType.hashCode();
+            }
+            return result;
+        }
     }
 }
