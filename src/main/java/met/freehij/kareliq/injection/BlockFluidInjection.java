72c78a77cd5bcddc47eedec340360138e9d3aeaf
this aint my pfp <sip22ssd@gmail.com>
2025-10-04 21:29:45 +0300

    i have no clue how to make optimizations

--- src/main/java/met/freehij/kareliq/injection/BlockFluidInjection.java
+++ src/main/java/met/freehij/kareliq/injection/BlockFluidInjection.java
@@ -6,12 +6,32 @@ import met.freehij.loader.annotation.Inject;
 import met.freehij.loader.annotation.Injection;
 import met.freehij.loader.util.InjectionHelper;
 import met.freehij.loader.util.Reflector;
+import met.freehij.loader.util.mappings.ClassMappings;
+import met.freehij.loader.util.mappings.FieldMappings;
+import met.freehij.loader.util.mappings.MethodMappings;
+import met.freehij.loader.util.mappings.util.MethodMapping;
 import org.lwjgl.input.Keyboard;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 
 @Injection("BlockFluid")
 public class BlockFluidInjection {
+    private static Reflector axisAlignedBBReflector;
+    private static final String thePlayer = FieldMappings.get(ClassMappings.MINECRAFT, "thePlayer");
+    private static final String gameSettings = FieldMappings.get(ClassMappings.MINECRAFT, "gameSettings");
+    private static final String keyBindSneak = FieldMappings.get(ClassMappings.get("GameSettings"), "keyBindSneak");
+    private static final String keyCode = FieldMappings.get(ClassMappings.get("KeyBinding"), "keyCode");
+    private static final String fallDistance = FieldMappings.get(ClassMappings.get("Entity"), "fallDistance");
+    private static final String isInWater = MethodMappings.get(ClassMappings.get("Entity"), "isInWater").method;
+    private static final String jump = MethodMappings.get(ClassMappings.get("EntityPlayer"), "jump").method;
+    private static final String minX = FieldMappings.get(ClassMappings.get("Block"), "minX");
+    private static final String minY = FieldMappings.get(ClassMappings.get("Block"), "minY");
+    private static final String minZ = FieldMappings.get(ClassMappings.get("Block"), "minZ");
+    private static final String maxX = FieldMappings.get(ClassMappings.get("Block"), "maxX");
+    private static final String maxY = FieldMappings.get(ClassMappings.get("Block"), "maxY");
+    private static final String maxZ = FieldMappings.get(ClassMappings.get("Block"), "maxZ");
+
     @Inject(method = "getBlockBrightness")
     public static void getBlockBrightness(InjectionHelper helper) {
         if (Brightness.INSTANCE.isToggled()) {
@@ -23,25 +43,26 @@ public class BlockFluidInjection {
     @Inject(method = "getCollisionBoundingBoxFromPool")
     public static void getCollisionBoundingBoxFromPool(InjectionHelper helper) throws ClassNotFoundException {
         if (!WaterWalking.INSTANCE.isToggled()) return;
-        Reflector player = InjectionHelper.getMinecraft().getField("thePlayer");
+        Reflector player = InjectionHelper.getMinecraft().getFieldRaw(thePlayer);
         if (Keyboard.isKeyDown(InjectionHelper.getMinecraft()
-                .getField("gameSettings")
-                .getField("keyBindSneak")
-                .getField("keyCode").getInt())) {
+                .getFieldRaw(gameSettings)
+                .getFieldRaw(keyBindSneak)
+                .getFieldRaw(keyCode).getInt())) {
             return;
         }
-        if (player.getField("fallDistance").getFloat() > 2.F) return;
-        if ((boolean) player.invoke("isInWater").get()) {
-            player.invoke("jump");
+        if (player.getFieldRaw(fallDistance).getFloat() > 2.F) return;
+        if ((boolean) player.invokeRaw(isInWater, new Class[0]).get()) {
+            player.invokeRaw(jump, new Class[0]);
             return;
         }
-        helper.setReturnValue(InjectionHelper.getClazz("AxisAlignedBB").invoke("getBoundingBoxFromPool",
-                (int) helper.getArg(2) + (double) helper.getSelf().getField("minX").get(),
-                (int) helper.getArg(3) + (double) helper.getSelf().getField("minY").get(),
-                (int) helper.getArg(4) + (double) helper.getSelf().getField("minZ").get(),
-                (int) helper.getArg(2) + (double) helper.getSelf().getField("maxX").get(),
-                (int) helper.getArg(3) + (double) helper.getSelf().getField("maxY").get(),
-                (int) helper.getArg(4) + (double) helper.getSelf().getField("maxZ").get()).get());
+        if (axisAlignedBBReflector == null) axisAlignedBBReflector = InjectionHelper.getClazz("AxisAlignedBB");
+        helper.setReturnValue(axisAlignedBBReflector.invoke("getBoundingBoxFromPool",
+                (int) helper.getArg(2) + (double) helper.getSelf().getFieldRaw(minX).get(),
+                (int) helper.getArg(3) + (double) helper.getSelf().getFieldRaw(minY).get(),
+                (int) helper.getArg(4) + (double) helper.getSelf().getFieldRaw(minZ).get(),
+                (int) helper.getArg(2) + (double) helper.getSelf().getFieldRaw(maxX).get(),
+                (int) helper.getArg(3) + (double) helper.getSelf().getFieldRaw(maxY).get(),
+                (int) helper.getArg(4) + (double) helper.getSelf().getFieldRaw(maxZ).get()).get());
         helper.setCancelled(true);
     }
 }
